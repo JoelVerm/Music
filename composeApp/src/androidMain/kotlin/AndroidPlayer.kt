6ba8b2895563092ed2b064da469b004bfca26a5b
Joël Vermeulen <joel.vermeulen5@outlook.com>
2024-06-05 16:14:06 +0200

    Removed service for now

--- composeApp/src/androidMain/kotlin/AndroidPlayer.kt
+++ composeApp/src/androidMain/kotlin/AndroidPlayer.kt
@@ -1,33 +1,22 @@
+import android.app.Notification
 import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
-import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.OptIn
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.NotificationCompat
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.NotificationUtil
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.source.ShuffleOrder
 import androidx.media3.session.MediaController
+import androidx.media3.session.MediaNotification
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
-import androidx.media3.session.SessionCommand
-import androidx.media3.session.SessionResult
 import androidx.media3.session.SessionToken
-import com.google.common.util.concurrent.ListenableFuture
-import com.google.common.util.concurrent.MoreExecutors
-import dev.flami.music.PlaybackService
 
 @Composable
 actual fun player(): State<Player> {
@@ -35,62 +24,54 @@ actual fun player(): State<Player> {
     return remember { mutableStateOf(AndroidPlayer(ctx)) }
 }
 
-class AndroidPlayer(context: Context) : Player {
-    private val sessionToken = SessionToken(context, ComponentName(context, PlaybackService::class.java))
-    private val controllerFuture = MediaController.Builder(context, sessionToken).buildAsync()
-    private var player: MediaController? = null
-
-    init {
-        controllerFuture.addListener(
-            {
-                player = controllerFuture.get().also { it.prepare() }
-            },
-            MoreExecutors.directExecutor()
-        )
-    }
+@OptIn(UnstableApi::class)
+class AndroidPlayer (context: Context) : Player {
+    private val player = ExoPlayer.Builder(context).build()
 
     private var playlist: List<Song> = listOf()
 
     override fun load(playlist: List<Song>) {
         this.playlist = playlist
-        player?.setMediaItems(playlist.map { MediaItem.fromUri(it.path) })
+        player.setMediaItems(playlist.map { MediaItem.fromUri(it.path) })
+        player.prepare()
+        player.seekToDefaultPosition(0)
     }
 
     override fun goto(song: Song) {
-        player?.seekToDefaultPosition(playlist.indexOf(song))
+        player.seekToDefaultPosition(playlist.indexOf(song))
     }
 
-    override fun currentSong(): Song? = player?.let { playlist.getOrNull(it.currentMediaItemIndex) }
+    override fun currentSong(): Song? = playlist.getOrNull(player.currentMediaItemIndex)
 
     override fun playing(state: Boolean) {
-        if (state == player?.isPlaying) return
-        if (state) player?.play()
-        else player?.pause()
+        if (state == player.isPlaying) return
+        if (state) player.play()
+        else player.pause()
     }
 
-    override fun playing(): Boolean = player?.isPlaying ?: false
+    override fun playing(): Boolean = player.isPlaying
 
     override fun seekTo(position: Int) {
-        player?.seekTo(position * 1000L)
+        player.seekTo(position * 1000L)
     }
 
     override fun progress(): Int {
-        return (player?.currentPosition?.toInt() ?: 0) / 1000
+        return (player.currentPosition.toInt()) / 1000
     }
 
     override fun next() {
-        player?.seekToNextMediaItem()
+        player.seekToNextMediaItem()
     }
 
     override fun previous() {
-        player?.seekToPreviousMediaItem()
+        player.seekToPreviousMediaItem()
     }
 
     override fun shuffle(state: Boolean) {
-        player?.shuffleModeEnabled = state
+        player.shuffleModeEnabled = state
     }
 
     override fun repeat(state: Boolean) {
-        player?.repeatMode = if (state) ExoPlayer.REPEAT_MODE_ALL else ExoPlayer.REPEAT_MODE_OFF
+        player.repeatMode = if (state) ExoPlayer.REPEAT_MODE_ALL else ExoPlayer.REPEAT_MODE_OFF
     }
 }
