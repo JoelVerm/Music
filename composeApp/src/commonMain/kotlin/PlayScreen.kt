553a5a73b621df96ce887f27557735311d80ccf0
Joël Vermeulen <joel.vermeulen5@outlook.com>
2024-06-14 00:44:30 +0200

    Fixed missing import

--- composeApp/src/commonMain/kotlin/PlayScreen.kt
+++ composeApp/src/commonMain/kotlin/PlayScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
